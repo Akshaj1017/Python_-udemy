00bd12da0a254f50850ed205d3c1dc50269e1569
Akshaj Agarwal <akshaj0304@gmail.com>
2021-12-28 22:31:39 +0530

    New add data_types

---
+++ Data_types.py
@@ -0,0 +1,36 @@
+#FUNDAMENTAL DATA TYPES
+#Data type is a value in python/store information
+'''
+1)int-integer
+2)float
+3)bool-bolean
+4)str-string
+5)list
+6)tuple
+7)set
+*)dict-dictionary
+'''
+
+#CLASSES->CUSTOM DATA TYPE->WE CAN CREATE OUR OWN DATA TYPES IN PYTHON
+
+#SPECIALIZED DATA TYPES->PACKAGES USED FROM LIBRARY->WE CAN ADD TO PYTHON USING MODULES
+
+#None->Nothing
+
+#1)INT AND FLOAT
+#AN INTEGER IS A NUMBER->WE USE INTEGER TO PERFROM MATHEMATICAL OPERATIONS->WHOLE NUMBERS
+'''
+print(type(2+4))->integer
+print(type(2-4))->integer
+print(type(2*4))->integer
+print(type(2/4))->float->FLAOTING POINT NUMBER->NUMBER WITH A DECIMAL POINT
+'''
+#FLOAT TAKES MORE SPACE IN MEMORY TO STORE RATHER THAN INTEGER
+#FLOAT+FLOAT->OUTPUT->FLOAT
+#INTEGER+FLOAT->OUTPUT->FLOAT
+'''
+print(2**2)->POWER
+print(2//4)->RETURNS ROUNDED DOWN TO INTEGER
+print(5%4)->REMAINDER OF THE DIVISION'''
+
+
