398234e580b050ba4da9d37f4eb9a9b5c605dcaa
Akshaj Agarwal <akshaj0304@gmail.com>
2021-12-30 01:15:41 +0530

    New add

--- Data_types.py
+++ Data_types.py
@@ -33,6 +33,30 @@ print(2**2)->POWER
 print(2//4)->RETURNS ROUNDED DOWN TO INTEGER
 print(5%4)->REMAINDER OF THE DIVISION'''
 
+#DATA TYPE->binary
+#binary->return binary conversion of number
+#print(bin(5))->output->0b101->0b represent its a binary number
+#print(int("0b101",2))->binary number converted to integer
+
+#STRING->PIECE OF TEXT->'',""
+print(type("Hi"))
+username="supecoder"
+password="supersecert"
+long_string='''
+W0W
+O O
+---
+'''
+print(long_string)
+
+first_name='Akshaj'
+last_name='Agarwal'
+full_name=first_name+" "+last_name
+print(full_name)
+
+#STRING CONCATENATION->ADDING STRINGS TOGETHER->ONly works with strings
+print('akshaj'+ 5)
+
 
 
 
---
+++ variable.py
@@ -0,0 +1,34 @@
+#VARIABLES->Store information that can be used in the program
+'''
+iq = 190->Value 190 is stored in variable name iq->Binding of value
+'''
+#BEST PRACTICES
+'''
+1)snake_case->readibility increase->eg>user_iq
+2)start with lowercase or underscore
+3)letter, number,underscores
+4)Case sensitive
+)Don't overwrite keywords
+'''
+#MAKE THE VARIABLE NAME REALLY DESCRIPTIVE
+
+#CONSTANT->VALUE DOEN'T CHANGE THROUGHTOUT THE PROGRAM
+PI=3.14
+
+#__ -> double underscore -> not us ein name if varibales -> dunder
+
+#a,b,c=1,2,3->Rapidly assign value to variables multiple times.
+
+#EXPRESSION VS STATEMENT
+'''
+iq = 100
+user_age = iq / 5)->expression only iq/5
+statement->the whole line-> user_age=iq/5
+'''
+
+#AUGMENTED ASSIGNMENT OPERATOR
+'''
+some_value=5
+some_value+=2
+print(some_value)
+'''
